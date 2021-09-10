c4e00684d795598c0284b715ff17eb777f862a8a
Milanotang <84255372+Milanotang@users.noreply.github.com>
2021-09-10 21:13:28 +0800

    Add files via upload

---
+++ 分支结构/swtich-case结构/Practice1.java
@@ -0,0 +1,21 @@
+package Switch;
+
+public class Practice1 {
+    public static void main(String[] args){
+        int a = 3;
+        int x = 100;
+        switch (a){
+            case 1:
+                x += 5;
+                break;
+            case 2:
+                x += 10;
+                break;
+            case 3:
+                x += 16;
+                break;
+            default:
+                x += 34;
+        }
+    }
+}
