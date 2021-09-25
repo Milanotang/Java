7bbc96130df9adfbb38d6a52098d870e109a7fde
Milanotang <84255372+Milanotang@users.noreply.github.com>
2021-09-25 09:59:13 +0800

    Delete 第四章 类/Pratice4 圆的面积 directory

--- 第四章 类/Pratice4 圆的面积/Test.java
+++
@@ -1,9 +0,0 @@
-package 第四章类.Practice4;
-
-public class Test {
-    public static void main(String[] args){
-        Circle circle1 = new Circle();
-        circle1.radium = 1;
-        circle1.findarea();
-    }
-}
